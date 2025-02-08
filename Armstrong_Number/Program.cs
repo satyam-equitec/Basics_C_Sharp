a5bef4a640580570d5c2ae98fb327375fd21bc82
satyam-equitec <satyam.equitec@gmail.com>
2025-02-08 13:16:06 +0530

    Armstrong Number
    An Armstrong number is defined as the sum of power of each digit to an digit number is equal to that number.

---
+++ Armstrong_Number/Program.cs
@@ -0,0 +1,15 @@
+﻿bool isAnagram(int a)
+{
+    int sum = 0;
+    int len = a.ToString().Length;
+    int r; int temp = a;
+    while (a > 0)
+    {
+        r = a % 10;
+        sum += Convert.ToInt32(Math.Pow(r,len));
+        a = a / 10;
+    }
+    return sum == temp;
+}
+Console.WriteLine(isAnagram(153));
+Console.WriteLine(isAnagram(1634));
